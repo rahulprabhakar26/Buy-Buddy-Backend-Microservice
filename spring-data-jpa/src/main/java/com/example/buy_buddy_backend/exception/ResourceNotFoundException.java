a6bfa4bae399b15f2b39de541c1efe02407ed29e
rahulprabhakar26 <prabhakarrahul2603@gmail.com>
2026-05-21 11:57:41 +0530

    update the springdatajps folder

--- spring-data-jpa/src/main/java/com/example/buy_buddy_backend/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.buy_buddy_backend.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- spring-data-jpa/src/main/java/com/example/buy_buddy_backend/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.buy_buddy_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.buy_buddy_backend.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
